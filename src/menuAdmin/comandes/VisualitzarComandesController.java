56d7f3c35dcf83b3badc26d8f02357acfac33d50
LordShadow <dammvila@gmail.com>
2018-05-31 10:49:34 +0200

    MostrarComandes

--- src/menuAdmin/comandes/VisualitzarComandesController.java
+++ src/menuAdmin/comandes/VisualitzarComandesController.java
@@ -1,15 +1,14 @@
 package menuAdmin.comandes;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import combos.Estat;
 import inici.ConnexioBD;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import objectes.Beguda;
 import objectes.Comanda;
 import objectes.Plat;
@@ -26,24 +25,29 @@ public class VisualitzarComandesController implements Initializable{
     @FXML
     ComboBox cbEstat;
     @FXML
-    TableView taula_comandes;
+    ListView<String> listComandes;
     @FXML
-    TableView taula_aliments;
+    ListView<String> listAliment;
     @FXML
-    TableView taula_begudes;
+    ListView<String> listBeguda;
+    @FXML
+    Label lTaula;
+    @FXML
+    Label lDia;
 
     Estat e = new Estat();
-    List<Comanda> llista_comandes = new ArrayList<Comanda>();
-    ObservableList<Comanda> llistaComandes = FXCollections.observableList(llista_comandes);
-    List<Plat> llista_aliments = new ArrayList<Plat>();
-    ObservableList<Plat> llistaAliments = FXCollections.observableList(llista_aliments);
-    List<Beguda> llista_begudes = new ArrayList<Beguda>();
-    ObservableList<Beguda> llistaBegudes = FXCollections.observableList(llista_begudes);
+    List<String> llista_comandes = new ArrayList<String>();
+    ObservableList<String> obsListComandes = FXCollections.observableList(llista_comandes);
+    List<String> llista_aliment = new ArrayList<String>();
+    ObservableList<String> obsListAliment = FXCollections.observableList(llista_aliment);
+    List<String> llista_beguda = new ArrayList<String>();
+    ObservableList<String> obsListBeguda = FXCollections.observableList(llista_beguda);
     ConnexioBD con = new ConnexioBD();
     Alert alerterror = new Alert(Alert.AlertType.ERROR);
     Alert alertconfirm = new Alert(Alert.AlertType.CONFIRMATION);
     ResultSet rs;
 
+    private String strIdComanda;
 
 
     @FXML
@@ -61,26 +65,20 @@ public class VisualitzarComandesController implements Initializable{
     }
 
     private void buscarComandes(boolean finalitzada) throws ClassNotFoundException, SQLException {
-        int id_comanda;
-        Date dia;
-        String nom_taula;
-        Boolean finalitzada_comanda;
+        obsListComandes.clear();
 
         if (finalitzada){
             System.out.println("Buscant a la base de dades...");
             try {
                 rs = con.queryDB(
-                        "select c.id_comanda as id_comanda, c.dia as dia, c.finalitzada as finalitzada, t.nom as nom_taula " +
-                                "from comandes c inner join taules t on c.id_taula = t.id_taula " +
+                        "select c.id_comanda as id_comanda, t.nom as nom_taula " +
+                                "from comanda c inner join taula t on c.id_taula = t.id_taula " +
                                 "where c.finalitzada = true order by dia"
                 );
                 while (rs.next()) {
-                    id_comanda = rs.getInt("id_comanda");
-                    dia = rs.getDate("dia");
-                    nom_taula = rs.getString("nom_taula");
-                    finalitzada_comanda = rs.getBoolean("finalitzada");
+                    String strComanda = rs.getString( "id_comanda");
 
-                    pujarATableviewComandes(id_comanda, dia, nom_taula, finalitzada_comanda);
+                    obsListComandes.add( strComanda);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -91,36 +89,35 @@ public class VisualitzarComandesController implements Initializable{
             System.out.println("Buscant a la base de dades...");
             try {
                 rs = con.queryDB(
-                        "select c.id_comanda as id_comanda, c.dia as dia, c.finalitzada as finalitzada, t.nom as nom_taula " +
-                                "from comandes c inner join taules t on c.id_taula = t.id_taula " +
+                        "select c.id_comanda as id_comanda, t.nom as nom_taula " +
+                                "from comanda c inner join taula t on c.id_taula = t.id_taula " +
                                 "where c.finalitzada = false order by dia"
                 );
                 while (rs.next()) {
-                    id_comanda = rs.getInt("id_comanda");
-                    dia = rs.getDate("dia");
-                    nom_taula = rs.getString("nom_taula");
-                    finalitzada_comanda = rs.getBoolean("finalitzada");
+                    String strComanda = rs.getString( "id_comanda");
 
-                    pujarATableviewComandes(id_comanda, dia, nom_taula, finalitzada_comanda);
+                    obsListComandes.add( strComanda);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+            listComandes.setItems( obsListComandes );
             System.out.println("Ha acavat de buscar a la base de dades");
         }
     }
 
 
-    private void pujarATableviewComandes(int id_comanda, Date dia, String nom_taula, boolean finalitzada_comanda) {
-        llista_comandes.add(new Comanda(id_comanda, dia, nom_taula, finalitzada_comanda));
-        taula_comandes.setItems(llistaComandes);
+    private void pujarATableviewComandes(String strComanda) {
+        llista_comandes.add( strComanda );
+        listComandes.setItems(obsListComandes);
     }
 
     private void borrarTableViews(){
         //llistaBegudes.clear();
         //taula_begudes.setItems(llistaBegudes);
         //llista_comandes.clear();
-        //taula_comandes.setItems(llistaComandes);
+        //taula_comandes.setItems(obsListComandes);
         //llista_aliments.clear();
         //taula_aliments.setItems(llistaAliments);
     }
@@ -130,5 +127,74 @@ public class VisualitzarComandesController implements Initializable{
         ObservableList<String> estat =
                 FXCollections.observableArrayList(e.getEstat());
         cbEstat.getItems().addAll(estat);
+
+        listComandes.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                strIdComanda = listComandes.getSelectionModel().getSelectedItem();
+                mostrarDades();
+                mostrarAliments();
+                mostrarBegudes();
+
+            }
+        });
+    }
+
+    private void mostrarBegudes() {
+        obsListBeguda.clear();
+        System.out.println("Buscant a la base de dades...");
+        String sQuery = "SELECT nom FROM begudes AS b INNER JOIN comandabeguda AS cb ON b.id_beguda = cb.id_beguda WHERE cb.id_comanda = " + strIdComanda;
+        try {
+            ResultSet rs = con.queryDB( sQuery);
+            while (rs.next()) {
+                String strBeguda = rs.getString( "nom");
+
+                obsListBeguda.add( strBeguda );
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Ha acavat de buscar a la base de dades");
+
+        listBeguda.setItems( obsListBeguda );
+    }
+
+    private void mostrarAliments() {
+        obsListAliment.clear();
+        System.out.println("Buscant a la base de dades...");
+        String sQuery = "SELECT nom FROM aliments AS a INNER JOIN comandaaliment AS ca ON a.id_aliment = ca.id_aliment WHERE ca.id_comanda = " + strIdComanda;
+        try {
+            ResultSet rs = con.queryDB( sQuery);
+            while (rs.next()) {
+                String strAliment = rs.getString( "nom");
+
+                obsListAliment.add( strAliment );
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Ha acavat de buscar a la base de dades");
+
+        listAliment.setItems( obsListAliment );
+    }
+
+    private void mostrarDades() {
+
+        String sQuery = "SELECT t.nom, c.dia FROM taula AS t, comanda AS c WHERE t.id_taula = c.id_taula AND c.id_comanda =" + strIdComanda;
+
+        try {
+            ResultSet rs = con.queryDB( sQuery );
+            if( rs.next()) {
+                lTaula.setText( rs.getString( "nom" ) );
+                lDia.setText( rs.getString("dia") );
+
+            }
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+
     }
 }
